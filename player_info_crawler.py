31f9ef0bfaeb2a3ec22c90941e23f96341916a40
Haoran Liang <33506345+harlan0103@users.noreply.github.com>
2019-03-21 14:35:53 -0500

    Update player info crawler
    Enable to get player images, birth, nation, nickname information

--- player_info_crawler.py
+++ player_info_crawler.py
@@ -4,6 +4,7 @@ import re
 import os
 from bs4 import BeautifulSoup
 from prettytable import PrettyTable
+import countryParser
 
 headers = {'User-Agent':'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6'}
 
@@ -23,6 +24,8 @@ def getTeamList():
 
 		# Call playerPic() to get all player picture
 		playerInfo(team_name, team_url)
+
+
 
 def playerInfo(teamname, teamurl):	
 	# We use header to pretend as explore
@@ -46,11 +49,11 @@ def playerInfo(teamname, teamurl):
 
 		# Set nick name as the name of img
 		name = teamname + '_' + nick_name + '.png'
-
+		path = 'img/' + name
 		# Get picture content as bytes and save into .jpg
 		piccontent = requests.get(src, headers = headers)
 
-		f = open(name, 'ab')
+		f = open(path, 'ab')
 		f.write(piccontent.content)
 		f.close()
 
@@ -66,12 +69,35 @@ def playerInfo(teamname, teamurl):
 		player_page = requests.get(liquidpedia_url, headers = headers)
 		player_page_data = BeautifulSoup(player_page.text, 'lxml')
 
-		# Get player birthday information
+		# Get player birthday and country information
 		player_info = player_page_data.select('body > div[id=wrap] > div > div[class=row] > div > div > div[id=bodyContent] > div[id=mw-content-text] > div[class=mw-parser-output] > div > div > div > div[class=infobox-cell-2] > span > span[class=bday]')
-		bd = player_info[0].get_text().split('-')
-		# player_nation = player_page_data.select('body > div[id=wrap] > div > div[class=row] > div > div > div[id=bodyContent] > div[id=mw-content-text] > div[class=mw-parser-output] > div > div > div > div[class=infobox-cell-2] > span > span[class=bday]')
-		birth = bd[0] + '.' + bd[1] + '.' + bd[2]
-		print(birth)
+		player_nation = player_page_data.select('body > div[id=wrap] > div > div[class=row] > div > div > div[id=bodyContent] > div[id=mw-content-text] > div[class=mw-parser-output] > div > div > div > div[class=infobox-cell-2] > a')
+
+		try:
+			bd = player_info[0].get_text().split('-')
+		except (IndexError):
+			birth = "1990.01.01"
+		else:
+			birth = bd[0] + '.' + bd[1] + '.' + bd[2]
+		
+		# Get 3-digit country code
+		try:
+			country_name = player_nation[0]['title']
+		except (IndexError):
+			country_code = "None"
+		else:
+			country_code = countryParser.parseCountryCode(country_name)
+		print(country_code)
+		# Now save player information as the .lua.txt format
+		firstName = player_name.split()[0]
+		lastName = player_name.split()[2]
+
+		player_detail = '{ firstName = "' + firstName + '", lastName = "' + lastName + '", nickName = "' + nick_name + '", birth = "' + birth + '", nation = "' + country_code + '", region = "", customfigure = "' + name + '",  },\n'
+
+		# Write player info detail to the .txt file
+		player_file = open('playerData.txt', mode='a')
+		player_file.write(player_detail)
+		player_file.close()
 
 if __name__ == '__main__':
 	getTeamList()
